99b25ad3a526767189d6a245ebca8e4e9bb08836
Gustavo Santos <gussanxz@gmail.com>
2025-12-28 04:41:21 -0400

    criando tela Vendas>Cadastros

---
+++ app/src/main/java/com/gussanxz/orgafacil/activity/vendas/VendasCadastrosActivity.java
@@ -0,0 +1,32 @@
+package com.gussanxz.orgafacil.activity.vendas;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.gussanxz.orgafacil.R;
+
+public class VendasCadastrosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_vendas_cadastros);
+        View main = findViewById(R.id.rootCadastros); // 2) precisa existir no layout
+        if (main == null) {
+            throw new IllegalStateException("View R.id.main não existe em activity_vendas_cadastros.xml");
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rootCadastros), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
