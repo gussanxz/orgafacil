d5b6864fe8dad1d6512abcbdd9f76e9f5fed97b8
Gustavo Santos <gussanxz@gmail.com>
2026-01-15 13:38:43 -0400

    (fix) corrigindo caminho da tela Nova Venda

--- app/src/main/java/com/gussanxz/orgafacil/activity/main/vendas/novavenda/RegistrarVendasActivity.java
+++ app/src/main/java/com/gussanxz/orgafacil/activity/main/vendas/novavenda/RegistrarVendasActivity.java
@@ -17,7 +17,7 @@ public class RegistrarVendasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_vendas_novavenda_registrarvendas);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.novaVenda), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
