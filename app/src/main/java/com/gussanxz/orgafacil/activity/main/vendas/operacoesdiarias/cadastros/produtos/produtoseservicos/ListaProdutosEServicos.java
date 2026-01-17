18ad4e71c0cec73777d57c220072996097fcd154
tomeki0 <theguilhermelimas@gmail.com>
2026-01-16 20:56:54 -0400

    (new) xml lista produtos e .class desse xml

---
+++ app/src/main/java/com/gussanxz/orgafacil/activity/main/vendas/operacoesdiarias/cadastros/produtos/produtoseservicos/ListaProdutosEServicos.java
@@ -0,0 +1,17 @@
+package com.gussanxz.orgafacil.activity.main.vendas.operacoesdiarias.cadastros.produtos.produtoseservicos;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.gussanxz.orgafacil.R;
+
+public class ListaProdutosEServicos extends AppCompatActivity {
+
+   /* @Override
+    protected void OnCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_produtos_e_servicos);
+    } */
+}
+
