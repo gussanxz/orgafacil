9446fa7e08aa4d586d9e9c24a151b70a3232d181
tomeki0 <theguilhermelimas@gmail.com>
2026-01-18 21:02:57 -0400

    (new) migracao da conexao pro firestore

--- app/src/main/java/com/gussanxz/orgafacil/config/ConfiguracaoFirestore.java
+++ app/src/main/java/com/gussanxz/orgafacil/config/ConfiguracaoFirestore.java
@@ -1,13 +1,15 @@
 package com.gussanxz.orgafacil.config;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class ConfiguracaoFirebase {
-    private static FirebaseFirestore firestore;
+public class ConfiguracaoFirestore {
 
+    // Instância estática para garantir que só exista uma conexão ativa (Singleton)
+    private static FirebaseFirestore firestore;
+    private static FirebaseAuth autenticacao;
+
+    // Metodo para obter a instância do Firestore e acessar o banco de dados
     public static FirebaseFirestore getFirestore() {
         if (firestore == null) {
             firestore = FirebaseFirestore.getInstance();
@@ -15,18 +17,7 @@ public class ConfiguracaoFirebase {
         return firestore;
     }
 
-    private static FirebaseAuth autenticacao;
-    private static DatabaseReference firebase;
-
-    //retornar a instancia do FirebaseDatabase
-    public static DatabaseReference getFirebaseDatabase(){
-        if ( firebase == null ) {
-            firebase = FirebaseDatabase.getInstance().getReference();
-        }
-        return firebase;
-    }
-
-    //retornar a instancia do FirebaseAuth
+    //Metodo para gerenciar login/cadastro de usuários e retornar a instancia do FirebaseAuth
     public static FirebaseAuth getFirebaseAutenticacao() {
         if( autenticacao == null) {
             autenticacao = FirebaseAuth.getInstance();
